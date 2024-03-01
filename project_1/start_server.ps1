f97da938995738c61077fbbd9fa06ccae87b869c
Kirill <dremina92@gmail.com>
2024-03-01 17:07:34 +0500

    start_server.ps1 change , addition time and log server

--- project_1/start_server.ps1
+++ project_1/start_server.ps1
@@ -1,1 +1,6 @@
-uvicorn main:app --reload
+$url = "http://127.0.0.1:8000/"
+$log_name = "log_server.txt"
+$currentTime = (Get-Date).ToString("`ndd-MM-yy HH:mm:")
+$currentTime | Out-File -FilePath ".\$log_name" -Append
+Start-Process $url
+uvicorn main:app --reload  | Tee-Object -FilePath $log_name -Append
